72cddc3efbeb4993633b84a71be21684b109d256
Gjorn <1328635598@qq.com>
2022-07-10 21:51:50 +0800

    [Add] ListNode-Print

--- base/ListNode.java
+++ base/ListNode.java
@@ -28,4 +28,12 @@ public class ListNode {
         }
         return dummy.next;
     }
+
+    public static void PrintList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
 }
